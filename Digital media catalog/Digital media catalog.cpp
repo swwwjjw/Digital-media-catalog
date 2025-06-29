9b741fbca77063f9162ba89157d1f4dd6d17a39d
swwwjjw <zhukegor2006@gmail.com>
2025-06-29 15:31:30 +0300

    First commit

---
+++ Digital media catalog/Digital media catalog.cpp
@@ -0,0 +1,12 @@
+﻿// Digital media catalog.cpp: определяет точку входа для приложения.
+//
+
+#include "Digital media catalog.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
